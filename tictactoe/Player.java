8468083749532b66d9e9d02bda4c95cf6c502551
Alonso del Arte <alonso.delarte@gmail.com>
2022-12-01 22:59:36 -0500

    Initial commit

---
+++ tictactoe/Player.java
@@ -0,0 +1,23 @@
+package tictactoe; 
+
+/**
+ * Enumeration class Player - write a description of the enum class here
+ *
+ * @author Alonso del Arte
+ * @version 0.1, 1 December 2022
+ */
+public enum Player {
+    
+    NOUGHTS('O'), CROSSES('X');
+    
+    private final char symbol;
+    
+    public char getSymbol() {
+        return this.symbol;
+    }
+
+    Player(char ch) {
+        this.symbol = ch;
+    }
+    
+}
